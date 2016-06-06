e6d8bf8eae433d0ecd7033a35dd003a5b8bad538
EnragedRabisu <kydawg7@hotmail.com>
2016-06-06 04:40:42 -0400

    Starting code based on Pahimar's Let's Mod series.

---
+++ src/main/java/com/rabisu/compressedfurnace/CompressedFurnace.java
@@ -0,0 +1,31 @@
+package com.rabisu.compressedfurnace;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="CompressedFurnace", name="Compressed Furnace", version="1.9.4-0.1")
+public class CompressedFurnace
+{
+    @Mod.Instance("CompressedFurnace")
+    public static CompressedFurnace instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
